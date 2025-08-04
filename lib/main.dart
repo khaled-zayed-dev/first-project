a28dceade2fe25922e3df5599b939a507daa1ab5
Khaled-Zayed-dev <khaledsalahzayed@gmail.com>
2025-08-04 20:39:29 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:project1/home_screen%20.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
